2f312d5e6a9613c7d7008e3ccdbbaf4c774ed54d
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-05-02 15:53:02 +0200

    Initial Commit

---
+++ SwimResults/SwimResultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimResultsApp.swift
+//  SwimResults
+//
+//  Created by Konrad Weiß on 02.05.24.
+//
+
+import SwiftUI
+
+@main
+struct SwimResultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
