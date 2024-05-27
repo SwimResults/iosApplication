af1ba7e0869a4d9b1759d2e2aa35fdfc034028a5
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-05-27 12:41:28 +0200

    first heat model implementations

---
+++ SwimResults/Code/Features/Common/Models/HeatModel.swift
@@ -0,0 +1,13 @@
+//
+//  HeatMode.swift
+//  SwimResults
+//
+//  Created by Konrad Weiß on 25.05.24.
+//
+
+import Foundation
+
+struct HeatModel: Codable, Hashable {
+    var _id: String
+    var dateStart: Date
+}
---
+++ SwimResults/Code/Features/Common/Models/StartModel.swift
@@ -0,0 +1,8 @@
+//
+//  StartModel.swift
+//  SwimResults
+//
+//  Created by Konrad Weiß on 25.05.24.
+//
+
+import Foundation
