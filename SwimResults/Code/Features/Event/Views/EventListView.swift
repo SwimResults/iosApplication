1e40188b8c87f5f3a01eae61710fdcc74ba3e8f5
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-07-12 04:31:28 +0200

    fix event list refreshs when going back the navigation stack; place unavailable view and spinner right

--- SwimResults/Code/Features/Event/Views/EventListView.swift
+++ SwimResults/Code/Features/Event/Views/EventListView.swift
@@ -13,58 +13,57 @@ struct EventListView: View {
     @ObservedObject private var viewModel = EventListViewModel()
     
     var body: some View {
-        ZStack {
-            if viewModel.fetching {
-                SpinnerView()
+        VStack {
+            if (viewModel.parts.isEmpty && !viewModel.fetching) {
+                ContentUnavailableView {
+                    Label("Keine Wettkämpfe", systemImage: "calendar")
+                } description: {
+                    Text("Für diese Veranstaltung wurden keine Wettkämpfe gefunden.")
+                }
             } else {
-                if (viewModel.parts.count > 0) {
-                    List {
-                        ForEach(viewModel.parts, id: \.self) { part in
-                            
-                            
-                            Section( content: {
-                                ForEach(part.events, id: \.self) {meetingEvent in
-                                    NavigationLink(destination: EventView(meetingEvent: meetingEvent)) {
-                                        HStack {
-                                            Text("\(meetingEvent.number)")
-                                                .frame(width: 30, height: 30)
-                                                .background(
-                                                    RoundedRectangle(cornerRadius: 15, style: .continuous).fill(Color.blue)
-                                                )
-                                                .foregroundStyle(.white)
-                                                .font(.caption)
-                                                .fontWeight(.bold)
-                                            VStack {
-                                                HStack {
-                                                    Text(meetingEvent.getEventName())
-                                                    Spacer()
-                                                }
-                                                HStack {
-                                                    Text("10 Läufe")
-                                                        .font(.caption)
-                                                    Spacer()
-                                                }
-                                                
+                List {
+                    ForEach(viewModel.parts, id: \.self) { part in
+                        Section( content: {
+                            ForEach(part.events, id: \.self) {meetingEvent in
+                                NavigationLink(destination: EventView(meetingEvent: meetingEvent)) {
+                                    HStack {
+                                        Text("\(meetingEvent.number)")
+                                            .frame(width: 30, height: 30)
+                                            .background(
+                                                RoundedRectangle(cornerRadius: 15, style: .continuous).fill(Color.blue)
+                                            )
+                                            .foregroundStyle(.white)
+                                            .font(.caption)
+                                            .fontWeight(.bold)
+                                        VStack {
+                                            HStack {
+                                                Text(meetingEvent.getEventName())
+                                                Spacer()
                                             }
-                                            .frame(maxWidth: .infinity, alignment: .leading)
+                                            HStack {
+                                                Text("10 Läufe")
+                                                    .font(.caption)
+                                                Spacer()
+                                            }
+                                            
                                         }
+                                        .frame(maxWidth: .infinity, alignment: .leading)
                                     }
-                                    
                                 }
-                            }, header: {
-                                Text("Abschnitt \(part.number)")
-                            })
-                        }
+                                
+                            }
+                        }, header: {
+                            Text("Abschnitt \(part.number)")
+                        })
                     }
-                    .listStyle(.sidebar)
-                    .refreshable {
-                        await viewModel.fetchEventParts()
-                    }
-                } else {
-                    ContentUnavailableView {
-                        Label("Keine Wettkämpfe", systemImage: "calendar")
-                    } description: {
-                        Text("Für diese Veranstaltung wurden keine Wettkämpfe gefunden.")
+                }
+                .refreshable {
+                    await viewModel.fetchEventParts()
+                }
+                .listStyle(.sidebar)
+                .overlay {
+                    if viewModel.fetching && viewModel.parts == [] {
+                        SpinnerView()
                     }
                 }
             }
@@ -77,6 +76,8 @@ struct EventListView: View {
 }
 
 #Preview {
-    EventListView()
-        .environmentObject(CurrentMeeting.example())
+    NavigationStack {
+        EventListView()
+            .environmentObject(CurrentMeeting.example())
+    }
 }
