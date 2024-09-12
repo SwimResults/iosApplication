88fb41f56015f15d503d06255069a1758591cc5b
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-09-12 16:48:19 +0200

    prevent athlete start list from contineously fetching events on scroll

--- SwimResults/Code/Features/Start/ViewModels/StartListEntryViewModel.swift
+++ SwimResults/Code/Features/Start/ViewModels/StartListEntryViewModel.swift
@@ -17,6 +17,15 @@ final class StartListEntryViewModel: ObservableObject {
     var config: StartListConfig?
     
     func fetchEventForStart(_ start: StartModel) async {
+        // only fetch if event is missing
+        if (start.event == nil) { return }
+        print("try to fetch event \(start.event!)")
+        if (meetingEvent != nil) {
+            print("found in cache, do not send request")
+            return
+        }
+        print("not in cache, fetching...")
+        
         fetchingEvent = true
         do {
             let event = try await getEventByMeetingAndNumber(start.meeting, start.event!)
