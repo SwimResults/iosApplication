a24a24ac393887cab96b992589543f8fed31210f
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-09-20 11:24:01 +0200

    notification view simple toggles

--- SwimResults/Code/Features/Settings/Views/NotificationSettingsView.swift
+++ SwimResults/Code/Features/Settings/Views/NotificationSettingsView.swift
@@ -9,10 +9,37 @@ import SwiftUI
 import UserNotifications
 
 struct NotificationSettingsView: View {
+    @State var isNotifyMyself: Bool = false
+
     let notificationCenter = UNUserNotificationCenter.current()
     
     var body: some View {
-        List {
+        Form {
+            
+            Toggle("Eigener Sportler", isOn: $isNotifyMyself)
+                .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                .onChange(of: isNotifyMyself) {
+                    
+                }
+            
+            Toggle("Favoriten", isOn: $isNotifyMyself)
+                .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                .onChange(of: isNotifyMyself) {
+                    
+                }
+            
+            Toggle("Veranstaltungen", isOn: $isNotifyMyself)
+                .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                .onChange(of: isNotifyMyself) {
+                    
+                }
+            
+            Toggle("Zeitplan", isOn: $isNotifyMyself)
+                .toggleStyle(SwitchToggleStyle(tint: .accentColor))
+                .onChange(of: isNotifyMyself) {
+                    
+                }
+            
             Button("Request Notification Access") {
                 Task {
                     do {
@@ -22,7 +49,6 @@ struct NotificationSettingsView: View {
                     }
                 }
             }
-            .buttonStyle(.borderedProminent)
         }.navigationTitle("Benachrichtigungen")
     }
 }
