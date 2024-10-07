7af03610a3e8cf991d39da958264de7e7df59c0c
Konrad Weiß <konrad@schwimmteamerzgebirge.de>
2024-10-07 12:18:37 +0200

    add appauth dependencies

---
+++ SwimResults/Code/Features/Settings/Views/TestLoginView.swift
@@ -0,0 +1,18 @@
+//
+//  TestLoginView.swift
+//  SwimResults
+//
+//  Created by Konrad Weiß on 07.10.24.
+//
+
+import SwiftUI
+
+struct TestLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestLoginView()
+}
